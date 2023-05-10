bfe046abb98ca8eaba076217a5622418cda54274
Nathan McCutchen <n8wm@icloud.com>
2023-05-10 13:07:53 -0700

    incomplete example

--- run_environment.py
+++ run_environment.py
@@ -1,11 +1,25 @@
 import gymnasium as gym
-env = gym.make("Ant-v4", render_mode="human")
+import mujoco
 
-observation, info = env.reset()
-for _ in range(1000):
-    action = env.action_space.sample()
-    observation, reward, terminated, truncated, info = env.step(action)
+def demo_program():
+    env = gym.make("Ant-v4", render_mode="human")
 
-    if terminated or truncated:
-        observation, info = env.reset()
-env.close()
+    observation, info = env.reset()
+    for _ in range(1000):
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+    env.close()
+
+def inchworm_program():
+    model = mujoco.MjModel.from_xml_path("inchworm.xml")
+    data = mujoco.MjData(model)
+    while data.time < 1:
+        mujoco.mj_step(model, data)
+        print(data.geom_xpos)
+
+
+if __name__ == "__main__":
+    inchworm_program()
