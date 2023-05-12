cb1aa670382ae68b09c69c5c7dab30ac847d7345
Nathan McCutchen <n8wm@icloud.com>
2023-05-11 18:13:49 -0700

    ground + launch_passive cm

--- run_environment.py
+++ run_environment.py
@@ -1,5 +1,8 @@
+import time
 import gymnasium as gym
-from mujoco import MjModel, MjData, viewer
+from mujoco import MjModel, MjData, viewer, mj_step
+
+
 def demo_program():
     env = gym.make("Ant-v4", render_mode="human")
 
@@ -12,10 +15,23 @@ def demo_program():
             observation, info = env.reset()
     env.close()
 
+
 def inchworm_program():
     model = MjModel.from_xml_path("inchworm.xml")
     data = MjData(model)
-    viewer.launch(model, data)
+    with viewer.launch_passive(model, data) as view:
+        start = time.time()
+        while view.is_running() and time.time() - start < 30:
+            step_start = time.time()
+            mj_step(model, data)
+
+            with view.lock():
+                # modify viewer options here
+                pass
+            view.sync()
+            time_until_next_step = model.opt.timestep - (time.time() - step_start)
+            if time_until_next_step > 0:
+                time.sleep(time_until_next_step)
 
 
 if __name__ == "__main__":
