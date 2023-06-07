a8659e1cedc2361ff172de9e64bf52efa2bc1f19
Grant Holland <gwholland3@gmail.com>
2023-06-06 17:03:40 -0700

    Remove custom agent code

--- run_environment.py
+++ run_environment.py
@@ -10,7 +10,6 @@ from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.base_class import BaseAlgorithm
 from tqdm import tqdm
 
-from agent import REINFORCE
 from inchworm import InchwormEnv
 
 from glfw import GLFWError
@@ -92,47 +91,6 @@ def run_simulation_with_sb3_agent(
     env.close()
 
 
-def run_simulation_with_custom_agent(render=False):
-    env = InchwormEnv(render_mode=("human" if render else "rgb_array"))
-    wrapped_env = gym.wrappers.RecordEpisodeStatistics(
-        env, 50
-    )  # Records episode-reward
-
-    total_num_episodes = int(5e3)
-
-    # Observation-space of Inchworm env
-    obs_space_dims = env.observation_space.shape[0]
-    # Action-space of Inchworm env
-    action_space_dims = env.action_space.shape[0]
-
-    agent = REINFORCE(obs_space_dims, action_space_dims)
-    reward_over_episodes = []
-
-    pbar = tqdm(range(total_num_episodes), unit="eps")
-
-    for episode in pbar:
-        # Must reset the env before making the first call to step()
-        observation, info = wrapped_env.reset()
-
-        done = False
-        while not done:
-            # Request an action from the agent
-            action = agent.sample_action(observation)
-
-            # Apply that action to the environment, store the resulting data
-            observation, reward, terminated, truncated, info = wrapped_env.step(action)
-
-            done = terminated or truncated
-
-        reward_over_episodes.append(wrapped_env.return_queue[-1])
-        agent.update()
-
-        avg_reward = int(np.mean(wrapped_env.return_queue))
-        pbar.set_description(f"ep {episode}, avgReward {avg_reward}")
-
-    env.close()
-
-
 def run_simulation_random():
     env = InchwormEnv(render_mode="human")
 
@@ -205,7 +163,6 @@ def get_action():
 
 if __name__ == "__main__":
     # run_simulation_random()
-    # run_simulation_with_custom_agent(True)
 
     # train_with_sb3_agent(          # train a new model with SAC
     #     model_name="inchworm_sac",
